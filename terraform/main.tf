88829e57dba5e6a41b287475483ae4a7732974f0
Sayali Upasani <sayaliupasani@gmail.com>
2019-08-01 00:17:56 -0700

    Edited main.tf to include s3 bucket with static web hosting and relevant details

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,46 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_s3" {
-	bucket = "website903840284755"
-	acl = "private"
+	bucket = "${var.domain_name}"
+	acl = "public-read"
+
+	policy = <<POLICY
+{
+
+ 	"Id": "Policy1564634274590",
+  	"Version": "2012-10-17",
+     "Statement": [
+        {
+           "Sid": "Stmt1564634270138",
+            "Action": ["s3:GetObject"],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::www.sayaliupasani.com/*",
+            "Principal": "*"
+         }
+     ]
+ }
+ POLICY
+ 	
+ 	website {
+ 	  index_document="index.html"
+ 	  error_document="404.html"
+ 	}
 
 	tags = {
 	  Name = "Website_bucket"
+	}
+}
+
+resource "null_resource" "upload_files_s3" {
+	provisioner "local-exec" {
+	  command = "aws s3 sync ${path.cwd}/../public s3://${aws_s3_bucket.website_s3.id}"
+	}
+}
+
+resource "null_resource" "delete_files_s3" {
+	provisioner "local-exec" {
+	  when = "destroy"
+	  command = "aws s3 rm s3://${aws_s3_bucket.website_s3.id} --recursive"
 	}
 }
 
