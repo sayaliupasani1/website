4f35f8685093cc77e37f61959bef8aa7d4523a77
Sayali Upasani <sayaliupasani@gmail.com>
2019-08-03 10:48:44 -0700

    Commented variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,9 @@
 
 #-----Define aws variables-----
 
-variable "aws_access_key" {}
+#variable "aws_access_key" {}
 
-variable "aws_secret_key" {}
+#variable "aws_secret_key" {}
 
 variable "region" {
 	default = "us-east-1"
